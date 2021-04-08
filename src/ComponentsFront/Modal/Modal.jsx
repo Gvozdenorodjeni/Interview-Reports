e2b14c8eb1330de07406a99bb2d1f726aa21d9ac
unknown <vladagvozdenovic1991@gmail.com>
2021-04-08 20:12:13 +0200

    a

--- src/ComponentsFront/Modal/Modal.jsx
+++ src/ComponentsFront/Modal/Modal.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const Modal = ({ open, children, onClose }) => {
+const Modal = ({ open, onClose }) => {
   if (!open) return null;
   return ReactDom.createPortal(
     <>
       <div className="overlay" onClick={onClose} />
-      <div className="modal">{children}</div>
+      <div className="modal">
+        <h2>John Doe</h2>
+        <hr />
+        <div className="modalInfo">
+          <div className="personalInfo">
+            <p>Company</p>
+            <h2>Google</h2>
+            <p>Interview Date</p>
+            <h2>20.12.2009</h2>
+            <p>Phase</p>
+            <h2>Tech</h2>
+            <p>Status</p>
+            <h2>Passed</h2>
+          </div>
+          <div className="modalNotes">
+            <p>Notes</p>
+            <p className="personalNote">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
+              voluptatum maxime officia eveniet alias, excepturi, incidunt non
+              quaerat cumque doloremque, odio itaque quas! Magni ducimus iste id
+              veritatis. Temporibus!
+            </p>
+          </div>
+        </div>
+      </div>
     </>,
     document.getElementById("portal")
   );
--- src/PagesFront/Candidate/Candidate.jsx
+++ src/PagesFront/Candidate/Candidate.jsx
@@ -11,31 +11,7 @@ const Candidate = (props) => {
   return (
     <div className="candidate">
       <i class="fas fa-eye" onClick={() => setIsOpen(true)}></i>
-      <Modal onClose={() => setIsOpen(false)} open={isOpen}>
-        <h2>John Doe</h2>
-        <hr />
-        <div className="modalInfo">
-          <div className="personalInfo">
-            <p>Company</p>
-            <h2>Google</h2>
-            <p>Interview Date</p>
-            <h2>20.12.2009</h2>
-            <p>Phase</p>
-            <h2>Tech</h2>
-            <p>Status</p>
-            <h2>Passed</h2>
-          </div>
-          <div className="modalNotes">
-            <p>Notes</p>
-            <p className="personalNote">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-              voluptatum maxime officia eveniet alias, excepturi, incidunt non
-              quaerat cumque doloremque, odio itaque quas! Magni ducimus iste id
-              veritatis. Temporibus!
-            </p>
-          </div>
-        </div>
-      </Modal>
+      <Modal onClose={() => setIsOpen(false)} open={isOpen}></Modal>
     </div>
   );
 };
