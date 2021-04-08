235d5bd4761e075d7aaca23e427034e081279993
BIT Student <classroom@bgit.rs>
2021-04-08 15:56:12 +0200

    modal fixed

--- src/ComponentsFront/Modal/Modal.jsx
+++ src/ComponentsFront/Modal/Modal.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const Modal = ({ open, children, onClose }) => {
+const Modal = ({ open, children, e, c, onClose }) => {
   if (!open) return null;
   return ReactDom.createPortal(
     <>
       <div className="overlay" onClick={onClose} />
-      <div className="modal">{children}</div>
+      <div className="modal">
+        <h2>{e.candidateName}</h2>
+        <hr />
+        <div className="modalInfo">
+          <div className="personalInfo">
+            <p>Company</p>
+            <h2>{e.companyName}</h2>
+            <p>Interview Date</p>
+            <h2>
+              {new Date(e.interviewDate).getDate() +
+                " " +
+                new Date(e.interviewDate).getMonth() +
+                1 +
+                " " +
+                new Date(e.interviewDate).getFullYear()}
+            </h2>
+            <p>Phase</p>
+            <h2>{e.phase}</h2>
+            <p>Status</p>
+            <h2>{e.status}</h2>
+          </div>
+          <div className="modalNotes">
+            <p>Notes</p>
+            <p className="personalNote">{e.note}</p>
+          </div>
+        </div>
+      </div>
     </>,
     document.getElementById("portal")
   );
--- src/PagesFront/Candidate/Candidate.jsx
+++ src/PagesFront/Candidate/Candidate.jsx
@@ -7,6 +7,12 @@ import Modal from "../../ComponentsFront/Modal/Modal";
 
 const Candidate = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [report, setReport] = useState(null);
+
+  const showModal = (rep) => {
+    setIsOpen(true);
+    setReport(rep);
+  };
 
   const candidate = props.candidates.find((e) => props.match.params.id == e.id);
   const reports = props.reports.filter(
@@ -61,35 +67,12 @@ const Candidate = (props) => {
               <td>{e.status}</td>
               <td>
                 <div className="candidate">
-                  <i class="fas fa-eye" onClick={() => setIsOpen(true)}></i>
-                  <Modal onClose={() => setIsOpen(false)} open={isOpen}>
-                    {console.log(e)}
-                    <h2>{candidate.name}</h2>
-                    <hr />
-                    <div className="modalInfo">
-                      <div className="personalInfo">
-                        <p>Company</p>
-                        <h2>{e.companyName}</h2>
-                        <p>Interview Date</p>
-                        <h2>
-                          {new Date(e.interviewDate).getDate() +
-                            " " +
-                            new Date(e.interviewDate).getMonth() +
-                            1 +
-                            " " +
-                            new Date(e.interviewDate).getFullYear()}
-                        </h2>
-                        <p>Phase</p>
-                        <h2>{e.phase}</h2>
-                        <p>Status</p>
-                        <h2>{e.status}</h2>
-                      </div>
-                      <div className="modalNotes">
-                        <p>Notes</p>
-                        <p className="personalNote">{e.note}</p>
-                      </div>
-                    </div>
-                  </Modal>
+                  <i class="fas fa-eye" onClick={() => showModal(e)}></i>
+                  <Modal
+                    e={report}
+                    onClose={() => setIsOpen(false)}
+                    open={isOpen}
+                  ></Modal>
                 </div>
               </td>
             </tr>
